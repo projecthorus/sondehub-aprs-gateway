f25c2d3c0030e6a88ac97ec8715706b7806bceef
xss <michaela@michaela.lgbt>
2022-04-13 07:27:04 +1000

    add some more blocked things

--- sondehub_aprs_gw/__init__.py
+++ sondehub_aprs_gw/__init__.py
@@ -52,22 +52,28 @@ logging.getLogger().addHandler(ch)
 
 # A list of tocalls which we know are from devices that are either pushing radiosonde packets,
 # or are from devices which are very unlikely to be on a balloon (e.g. mobile radios)
-BLOCKED_TOCALLS = [
-    'APHAX0', # # {'raw': 'SQ5AAG-11>APHAX0,TCPIP*,qAC,T2ROMANIA:@231308h5224.50N/02057.30EO180/000/A=000940 Clb=0m/s P=0hPa T=0oC U=0% ID=T1550189 [404.50MHz]', 'from': 'SQ5AAG-11', 'to': 'APHAX0', 'path': ['TCPIP*', 'qAC', 'T2ROMANIA'], 'via': 'T2ROMANIA', 'messagecapable': True, 'raw_timestamp': '231308h', 'timestamp': 1641769988, 'format': 'uncompressed', 'posambiguity': 0, 'symbol': 'O', 'symbol_table': '/', 'latitude': 52.40833333333333, 'longitude': 20.955, 'course': 180, 'speed': 0.0, 'altitude': 286.512, 'comment': 'Clb=0m/s P=0hPa T=0oC U=0% ID=T1550189 [404.50MHz]'}
-    'APAT51-1', # Anytone AT-D578UV APRS mobile radio
-]
+BLOCKED_TOCALLS = (
+    'APHAX', # # {'raw': 'SQ5AAG-11>APHAX0,TCPIP*,qAC,T2ROMANIA:@231308h5224.50N/02057.30EO180/000/A=000940 Clb=0m/s P=0hPa T=0oC U=0% ID=T1550189 [404.50MHz]', 'from': 'SQ5AAG-11', 'to': 'APHAX0', 'path': ['TCPIP*', 'qAC', 'T2ROMANIA'], 'via': 'T2ROMANIA', 'messagecapable': True, 'raw_timestamp': '231308h', 'timestamp': 1641769988, 'format': 'uncompressed', 'posambiguity': 0, 'symbol': 'O', 'symbol_table': '/', 'latitude': 52.40833333333333, 'longitude': 20.955, 'course': 180, 'speed': 0.0, 'altitude': 286.512, 'comment': 'Clb=0m/s P=0hPa T=0oC U=0% ID=T1550189 [404.50MHz]'}
+    'APAT51', # Anytone AT-D578UV APRS mobile radio
+    'APDR', # APRSDroid
+)
 
 def isHam(thing):
     if "SONDEGATE" in thing["path"]: # {'raw': 'T1310753>APRARX,SONDEGATE,TCPIP,qAR,DF7OA-12:/233445h5242.24N/00959.93EO152/042/A=043155 Clb=3.7m/s t=-55.5C 405.701 MHz Type=RS41-SGP Radiosonde auto_rx v1.3.2 !w,%!', 'from': 'T1310753', 'to': 'APRARX', 'path': ['SONDEGATE', 'TCPIP', 'qAR', 'DF7OA-12'], 'via': 'DF7OA-12', 'messagecapable': False, 'raw_timestamp': '233445h', 'timestamp': 1641771285, 'format': 'uncompressed', 'posambiguity': 0, 'symbol': 'O', 'symbol_table': '/', 'latitude': 52.70402014652015, 'longitude': 9.99884065934066, 'course': 152, 'speed': 77.784, 'altitude': 13153.644, 'daodatumbyte': 'W', 'comment': 'Clb=3.7m/s t=-55.5C 405.701 MHz Type=RS41-SGP Radiosonde auto_rx v1.3.2'}
         return False
-    if thing["to"] in BLOCKED_TOCALLS: 
+    if thing["to"].startswith(BLOCKED_TOCALLS): 
         return False
     if "NSM is Not Sonde Monitor" in thing["comment"]: # {'raw': 'NSM20-11>APPMSP,F1ZWR-3*,WIDE2-2,qAR,F1ZNT-3:!4351.00N/00424.77EO169/023 Balloon is climbing   Ubatt 2.8V   NSM is Not Sonde Monitor', 'from': 'NSM20-11', 'to': 'APPMSP', 'path': ['F1ZWR-3*', 'WIDE2-2', 'qAR', 'F1ZNT-3'], 'via': 'F1ZNT-3', 'messagecapable': False, 'format': 'uncompressed', 'posambiguity': 0, 'symbol': 'O', 'symbol_table': '/', 'latitude': 43.85, 'longitude': 4.412833333333333, 'course': 169, 'speed': 42.596000000000004, 'comment': 'Balloon is climbing   Ubatt 2.8V   NSM is Not Sonde Monitor'}
         return False
-    if "Sonde" in thing["comment"]: # Generic filter for radiosonde packets. This may result in some false-positives.
+    if "SondeID" in thing["comment"]: # Sonde monitor
         return False
-    if "sonde" in thing["comment"]: # Generic filter for radiosonde packets. This may result in some false-positives.
+    if "Ozonesonde" in thing["comment"]: 
         return False
+    if "Recupero Radiosonde" in thing["comment"]: # "radiosonde recovery"
+        return False
+    if 'mbits' in thing: # mic-e - likely not a balloon
+        return False 
+    
     return True
 
 def parser(x):
